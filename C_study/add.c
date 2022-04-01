b4da32a9a3675da791a9e7ed747dacc693504616
costar011 <dnaer456@gmail.com>
2022-04-01 12:19:22 +0900

    77p

---
+++ C_study/add.c
@@ -0,0 +1,13 @@
+/* 쉽게 풀어쓴 C언어 express */
+
+#include <stdio.h>
+
+int main()
+{
+  printf("2+5=%d\n", 2+ 3);
+  printf("2-3=%d\n", 2 - 3);
+  printf("2*3=%d\n", 2 * 3);
+  printf("23=%d\n", 2 / 3);
+
+    return 0;
+}
