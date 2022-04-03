a6a9a18bedf249018caa2ccd766fc72557ff3f76
costar011 <dnaer456@gmail.com>
2022-04-03 22:16:19 +0900

    96p

---
+++ C_study/add1_1.c
@@ -0,0 +1,19 @@
+/* 쉽게 풀어쓴 C언어 express 96p */
+
+#include <stdio.h>
+
+int main(void)
+
+{
+ int x;
+ int y;
+ int sum;
+
+ x = 100;
+ y = 200;
+ sum = x + y;
+
+ printf("두수의 합  : %d", sum);
+
+ return 0;
+}
